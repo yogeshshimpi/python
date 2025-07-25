5d9fcc47c42c9e23488ec5e67884879409c97c82
Yogesh-shimpi <yogeshshimpi320@gmail.com>
2025-07-25 13:09:24 +0530

    Chapter 1 practice program

---
+++ Chapter 1/practice set/pr1.py
@@ -0,0 +1,11 @@
+#In this program i write paragraph and displyed using print function
+
+
+print('''
+      Python is a high-level, interpreted, general-purpose programming language.
+      Created by Guido van Rossum and first released in 1991, it is known for its readability and simple syntax,
+      which often allows developers to write programs with fewer lines of code compared to other languages.''')
+
+
+
+#using trpile single cot you can print mulitple line in single print function
---
+++ Chapter 1/practice set/pr2.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+speak = pyttsx3.init()
+speak.say("Hello my dear friend.")
+speak.runAndWait()
+
+
+#I import pyttsx3 module 
+# when we give a text then it read this text
