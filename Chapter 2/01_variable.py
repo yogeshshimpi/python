972af4f5c18af93005c033fbaedc99801e0aae62
Yogesh-shimpi <yogeshshimpi320@gmail.com>
2025-07-25 16:06:37 +0530

    In the Chapter 2 i learn what is the variable and datatype.

---
+++ Chapter 2/01_variable.py
@@ -0,0 +1,9 @@
+a = 10
+b = 10.30
+c = "yogesh"
+
+
+# In python the variable are the name of value where it store
+# means 10 is store in a means 10 name is now a 
+# variable are unique n the program.
+# The name of variable it should not repeat
---
+++ Chapter 2/02_datatype.py
@@ -0,0 +1,12 @@
+# THe datatype is a type of variable means it show which type of variable is in variable
+
+a = 10          #Integer datatype
+b = 10.30       #Floating datatype
+c = "yogesh"    #String datatype
+d = True        #Boolean datatype
+
+
+print(type(a))  #The type() function are used to check type of variable
+
+
+# Output: <class 'int'> This is the result of type(a)
