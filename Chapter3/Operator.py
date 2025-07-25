f3c180d39f86096e0d1ee1ccec99bf1aca99f4c0
Yogesh-shimpi <yogeshshimpi320@gmail.com>
2025-07-25 17:35:55 +0530

    In Chapter 3 i learn about the operator.

---
+++ Chapter3/Operator.py
@@ -0,0 +1,38 @@
+a = 10
+b = 2
+
+# Arithamatic operator
+
+
+print(a + b)  # adding a and b. Output: 6
+print(a - b)  # subtracting a and b. Output: 2
+print(a * b)  # multiplying a and b. Output: 8
+print(a / b)  # dividing a and b. Output: 2.0
+print(a % b)  # It return the reminder. Output: 0
+print(a ** b) # It multiply 4 2 times like square. Output: 16
+
+
+#Comparison operator
+
+# This operator  are use to compare the two operand
+
+print(a==b)   # Both operand value is same then return True else false
+print(a<b)    # a is less than b then return True else false
+print(a>b)    # a is greater than b then return True else false
+print(a<=b)   # a is less than and equal to b then return True else false
+print(a>=b)   # a is greater than and equal to b then return True else false
+print(a!=b)   # a is not equal to b then return True else false
+
+
+#Assignment operator
+
+# This operator are used to assign the value to the variable
+
+c = a # the right hand side value are assign to the left side value means  value of a is assign to the variable c
+print(c)
+
+#Logical operator 
+
+print(a>b and a<b)  #both condition is true then return true else false. Output: Flase
+print(a>b or a<b)   #from both condition one of them is true  then return true else false. Output: True
+print(not(a>b))     #if condition is true then return false and if condition is false then return true. Output: False
