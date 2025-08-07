216320720cfb01af4aed14a13ea1424272e9d6eb
Yogesh-shimpi <yogeshshimpi320@gmail.com>
2025-08-07 13:45:18 +0530

    Learning the loop in python like for and while loop.

---
+++ 08_Chpater 7 Loop/01_for_loop.py
@@ -0,0 +1,4 @@
+table = int(input("Enter the num: "))
+print("Table of ",table,": ")
+for i in range(1,11):
+    print(table ," * " ,i, " = ",table * i)
---
+++ 08_Chpater 7 Loop/02_while_loop.py
@@ -0,0 +1,6 @@
+table = int(input("Enter the num: "))
+print("Table of ",table,": ")
+i = 1
+while i<11:
+    print(table ," * " ,i, " = ",table * i)
+    i+=1
